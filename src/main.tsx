11249dc47591c9451db19e5158b162673e64b405
Pradanajati Aryawibowo <pradanajatiaryawibowo@gmail.com>
2026-06-24 19:28:48 +0700

    Fix LanguageProvider runtime issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import { LanguageProvider } from './lib/LanguageContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 );
